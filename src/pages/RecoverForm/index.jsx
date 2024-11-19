5dd193bcfc79f46f0cd8bc1dadf745146cab3c1a
Thiago Allisson <thiago.allisson@ufpi.edu.br>
2024-11-19 00:28:09 -0300

    Add comments RecoverForm

--- src/pages/RecoverForm/index.jsx
+++ src/pages/RecoverForm/index.jsx
@@ -26,7 +26,7 @@ const RecoverForm = () => {
               />
             </div>
 
-            {/* Botão de Enviar */}
+            {/* Submit Button */}
             <button
               type="submit"
               className="w-full bg-indigo-600 text-white py-2 px-4 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
