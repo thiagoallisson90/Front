41e7368eea24d3a286f9214d64eea278d821c7d2
Thiago Allisson <thiago.allisson@ufpi.edu.br>
2024-11-22 18:40:20 -0300

    Fix Logo

--- src/components/Logo/index.jsx
+++ src/components/Logo/index.jsx
@@ -4,7 +4,7 @@ import logo from "../../imgs/logo4.jpeg";
 const Logo = () => {
   return (
     <>
-      <Link to="/" className="text-white text-2xl font-bold">
+      <Link to="/home" className="text-white text-2xl font-bold">
         <img src={logo} className="w-44" />
       </Link>
     </>
