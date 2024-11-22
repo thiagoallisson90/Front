dac24cf8d65e1796b793dfe3262f5541f1dc2adf
Thiago Allisson <thiago.allisson@ufpi.edu.br>
2024-11-22 03:37:21 -0300

    Load image logo from import

--- src/components/Logo/index.jsx
+++ src/components/Logo/index.jsx
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom";
+import image from "../../../public/logo4.jpeg";
 
 const Logo = () => {
   return (
     <>
       <div className="flex justify-center">
         <Link to="/" className="text-white text-2xl font-bold">
-          <img src="../public/logo4.jpeg" className="w-44" />
+          <img src={image} className="w-44" />
         </Link>
       </div>
     </>
