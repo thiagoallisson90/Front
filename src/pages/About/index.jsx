924c3efe8ff3f1b2fbe8ccb923fc83bf929f6d12
Thiago Allisson <thiago.allisson@ufpi.edu.br>
2024-11-22 02:56:00 -0300

    Fix Top in About

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,5 +1,7 @@
 import { Helmet, HelmetProvider } from "react-helmet-async";
-import NavBar from "../../components/NavBar";
+import Footer from "../../components/Footer";
+import Layout from "../../components/Layout";
+import Top from "../../components/Top";
 
 const About = () => {
   const pageName = "About";
@@ -12,11 +14,45 @@ const About = () => {
         </Helmet>
       </HelmetProvider>
 
-      <NavBar />
+      <Layout>
+        <div className="flex justify-center">
+          <Top />
+        </div>
 
-      <main className="container mx-auto px-4">
-        <h1 className="font-medium">{pageName}</h1>
-      </main>
+        <section className="py-24 relative">
+          <div className="w-full max-w-7xl px-4 md:px-5 lg:px-5 mx-auto">
+            <div className="w-full justify-start items-center gap-8 grid lg:grid-cols-2 grid-cols-1">
+              <div className="w-full flex-col justify-start lg:items-start items-center gap-10 inline-flex">
+                <div className="w-full flex-col justify-start lg:items-start items-center gap-4 flex">
+                  <h2 className="text-gray-900 text-4xl font-bold font-manrope leading-normal lg:text-start text-center">
+                    Building Stronger Communities through Collaboration and
+                    Empowerment
+                  </h2>
+                  <p className="text-gray-500 text-base font-normal leading-relaxed lg:text-start text-center">
+                    Through collaborationperse perspectives and strengths are
+                    leveraged to create inclusive environments where everyone
+                    has the opportunity to thrive. This approach not only
+                    fosters personal growth and achievement but also strengthens
+                    the fabric of society.
+                  </p>
+                </div>
+                <button className="sm:w-fit w-full px-3.5 py-2 bg-indigo-600 hover:bg-indigo-800 transition-all duration-700 ease-in-out rounded-lg shadow-[0px_1px_2px_0px_rgba(16,_24,_40,_0.05)] justify-center items-center flex">
+                  <span className="px-1.5 text-white text-sm font-medium leading-6">
+                    Get Started
+                  </span>
+                </button>
+              </div>
+              <img
+                className="lg:mx-0 mx-auto h-full rounded-3xl object-cover"
+                src="https://pagedone.io/asset/uploads/1717751272.png"
+                alt="about Us image"
+              />
+            </div>
+          </div>
+        </section>
+
+        <Footer />
+      </Layout>
     </>
   );
 };
