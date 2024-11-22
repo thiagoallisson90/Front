5b5a57abe24364185e5dee5837bb2c3576f822fd
Thiago Allisson <thiago.allisson@ufpi.edu.br>
2024-11-22 02:25:16 -0300

    Add Helmet and title to Home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,15 @@
 import NavBar from "../../components/NavBar";
 import DataGridP from "../../components/DataGridP";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
 const Home = () => {
   return (
     <>
+      <HelmetProvider>
+        <Helmet>
+          <title>OptimusLoRa - Home</title>
+        </Helmet>
+      </HelmetProvider>
       <NavBar />
 
       <DataGridP />
